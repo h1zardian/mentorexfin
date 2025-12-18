b67bbdc5bc6388f1e0c90c636b67202a657ccdd5
edgseu <amanb.singh21@gmail.com>
2025-12-18 19:06:42 +0530

    initial commit

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,32 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y copr enable peterwu/rendezvous
+
+# Base Packages
+PACKAGES=(
+    bibata-cursor-themes
+    papirus-icon-theme
+    gparted
+    java-25-openjdk
+)
+
+
+# Remove Unneeded and Disable Repos
+# UNINSTALL_PACKAGES=(
+
+# )
+
+dnf5 install -y --allowerasing \
+    --setopt=install_weak_deps=False \
+    -x bluefin-readymade-config \
+    "${PACKAGES[@]}"
+
+
+# dnf5 remove -y "${UNINSTALL_PACKAGES[@]}"
 
 # Use a COPR Example:
-#
+
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
@@ -21,4 +43,7 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+
+# Removing starship from system bashrc bashconfig, becuse I don't like bling in bash.
+sed -i.bak '/starship init bash/s/^/# /' /etc/bashrc
